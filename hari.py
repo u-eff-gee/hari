00e0d09c72e0cff376a72624d6504116b6538a08
Udo Gayer <ugayer@ikp.tu-darmstadt.de>
2018-01-02 14:37:57 +0100

    Fix for command-line execution

--- hari.py
+++ hari.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+
 # Copyright 2017 Udo Gayer (gayer.udo@gmail.com)
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -11,8 +13,6 @@
 #
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
-#!/usr/bin/env python3
 
 import numpy as np
 import matplotlib.pyplot as plt
